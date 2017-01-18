35dbba62cd3635219e352253b0b0a92144ac3d28
SjoerdKrabben <sjoerdkrabben@gmail.com>
2017-01-18 22:08:07 +0100

    more database call changes

--- includes/database.php
+++ includes/database.php
@@ -5,7 +5,7 @@ $password = "JPSm5iau4SFAIr8C";
 $serverName = "localhost";
 $database = "Muziekwinkel";
 
-$conn = mysqli_connect($serverName, $username, $password);
+$conn = new mysqli($serverName, $username, $password);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
