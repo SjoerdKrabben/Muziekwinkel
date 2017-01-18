f8249bbcbc5a7429c58f1f9449da00f31bb1360a
SjoerdKrabben <sjoerdkrabben@gmail.com>
2017-01-18 14:48:57 +0100

    more database call changes

--- includes/database.php
+++ includes/database.php
@@ -4,12 +4,13 @@ $userName = "root";
 $password = "JPSm5iau4SFAIr8C";
 $serverName = "localhost";
 $database = "Muziekwinkel";
+$connectionInfo = array("Database" => "Muziekwinkel", "UID" => "root", "PWD"=>"JPSm5iau4SFAIr8C");
 
 
-$conn = mysqli_connect("mysql:host=$servername;dbname=$database", $userName, $password);
+$conn = mysqli_connect($serverName, $connectionInfo);
 
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error() . print_r( sqlsrv_errors(), true));
 }
 echo "Connected successfully";
 
