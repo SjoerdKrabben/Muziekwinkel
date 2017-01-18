3e411dff32909b44128ddc58661702ea2c2a7578
SjoerdKrabben <sjoerdkrabben@gmail.com>
2017-01-18 14:52:38 +0100

    more database call changes

--- includes/database.php
+++ includes/database.php
@@ -1,17 +1,29 @@
 <?php
 
-$userName = "root";
+$username = "root";
 $password = "JPSm5iau4SFAIr8C";
 $serverName = "localhost";
 $database = "Muziekwinkel";
 $connectionInfo = array("Database" => "Muziekwinkel", "UID" => "root", "PWD"=>"JPSm5iau4SFAIr8C");
 
 
-$conn = mysqli_connect($serverName, $connectionInfo);
+//$conn = mysqli_connect($serverName, $connectionInfo);
+//
+//if (!$conn) {
+//    die("Connection failed: " . mysqli_connect_error());
+//}
+//echo "Connected successfully";
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
 }
-echo "Connected successfully";
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
 
 ?>
